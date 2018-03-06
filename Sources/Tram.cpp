942bd77327ceabee8a05511fff1c4d486ed14c9f
OwDemShad <galo.guccy@gmail.com>
2018-03-06 14:01:51 +0100

    Tram.cpp : 4 méthodes
    - void avance() : fait avancer le tram selon sa vitesse (détermine ses coordonnées)
    - double distanceTramDevant() : indique la distance avec le tram juste devant
    - bool doitSArreter() : renvoie vrai si distanceTramDevant <= d_distanceMin, false sinon (ne fait aucun changement de vitesse)
    - void arret() : met à 0 la vitesse du tram

--- Sources/Tram.cpp
+++ Sources/Tram.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "..\Headers\Tram.h"
+#include <cmath>
 
 Tram::Tram() : d_vitesse{ false }, d_sens{ false }, d_tempsArret{ 1000 }, d_distanceMin{ 100 },
 d_vitesseMax{ 20 }, d_position{}, d_tramSuiv{ nullptr }, d_arretSuiv{ nullptr },
@@ -103,4 +104,61 @@ void Tram::setTramSuivant(Tram &tramSuivant)
 void Tram::setArretSuivant(Arret &arretSuivant)
 {
 	*d_arretSuiv = arretSuivant;
+}
+
+void Tram::avance()
+{
+	// 1. Détermination du nombre de pixels dont le tram doit avancer
+
+	// variation de x et y pour changer les coordonnées du tram (th. Pythagore)
+	double variation = sqrt((pow(d_vitesse, 2) / 2));
+
+
+	// 2. Changement de la position du tram
+
+	// position de l'arrêt vers lequel se dirige le tram
+	Position pArretSuiv = d_arretSuiv->getPosition();
+
+	if (d_sens)	// avance de gauche à droite (d_sens = true)
+	{	
+		// cas où la droite entre les deux arrêts autour du tram est décroissante 
+		if (pArretSuiv.getX() > d_position.getX() && pArretSuiv.getY() > d_position.getY())		// <==> prochain arrêt visuellement en-dessous du tram
+		{
+			d_position.setPos(d_position.getX() + variation, d_position.getY() + variation);
+		}
+		// cas où la droite entre les deux arrêts autour du tram est croissante
+		else if (pArretSuiv.getX() > d_position.getX() && pArretSuiv.getY() < d_position.getY())	// <==> prochain arrêt visuellement au-dessus du tram
+		{
+			d_position.setPos(d_position.getX() + variation, d_position.getY - variation);
+		}
+	}
+	else	// avance de droite à gauche (d_sens = false)
+	{
+		// droite décroissante (prochain arêt en-dessous du tram)
+		if (pArretSuiv.getX() < d_position.getX() && pArretSuiv.getY() > d_position.getY())
+		{
+			d_position.setPos(d_position.getX() - variation, d_position.getY() + variation);
+		}
+		// droite croissante (prochain arrêt au-dessus du tram)
+		else if (pArretSuiv.getX() < d_position.getX() && pArretSuiv.getY() < d_position.getY())
+		{
+			d_position.setPos(d_position.getX() - variation, d_position.getY() - variation);
+		}
+	}
+}
+
+double Tram::distanceTramDevant() const
+{
+	double distance = sqrt(pow( (d_tramSuiv->d_position.getX() - d_position.getX()), 2) + pow((d_tramSuiv->d_position.getY() - d_position.getY()), 2));
+	return distance;
+}
+
+bool Tram::doitSArreter()
+{
+	return distanceTramDevant() <= d_distanceMin;
+}
+
+void Tram::arret()
+{
+	setVitesse(false);
 }
