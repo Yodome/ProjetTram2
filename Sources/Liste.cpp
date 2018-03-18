f18f9428bdc705374b23438c3b53bc4268f11112
LPR00t <36168923+LPR00t@users.noreply.github.com>
2018-03-18 15:22:46 +0100

    Add files via upload
    Je n'ai pas encore intégré les commentaires Doxygen je les ferai plus tard

--- Sources/Liste.cpp
+++ Sources/Liste.cpp
@@ -4,107 +4,108 @@
 
 #include "..\Headers\Liste.h"
 
-Liste::Liste() : d_taille{0}, d_arretTete{ nullptr }, d_arretQueue{ nullptr }
+Liste::Liste() : d_taille{0}, d_arretTete{ nullptr }, d_arretQueue{ nullptr }   //Initialisation des variables privées par défaut (liste vide donc = 0 et pas de tete ni de queue)
 {}
 
-Liste::Liste(Liste & l)
+Liste::Liste(Liste & l) //Constructeur par recopie
 {
-	Arret *a2 = l.d_arretTete, *prec1 = 0, *a1 = 0;
+	Arret *a2 = l.d_arretTete, *prec1 = 0, *a1 = 0;    //On crée un arrêt a2 qui va nous permettre de parcourir la liste passée en paramètre sans perdre la tête,
+                                                       //on crée un arrêt prec1 qui sera le précédent et un arrêt a1 qui est l'arrêt courant dans la liste this
 
-	while (a2 != 0)
-	{
-		a1 = new Arret();
+	while (a2 != 0)    //Tant que la tête n'est pas nulle
+	{ 
+		a1 = new Arret(); //On crée un nouvel arrêt
 		
-		if (prec1 != 0)
+		if (prec1 != 0)   //Si le précédent n'est pas nul
 		{
-			prec1->d_arretSuiv = a1;
-			a1->d_arretPrec = prec1;
+			prec1->d_arretSuiv = a1; //le suivant de précédent devient a1
+			a1->d_arretPrec = prec1; //le précédent de a1 devient prec1
 		}
-		else
+		else  //Sinon 
 		{
-			d_arretTete = a1;
+			d_arretTete = a1;    //a1 devient la tête
 		}
 
-		a2 = a2->d_arretSuiv;
-		prec1 = a1;
+		a2 = a2->d_arretSuiv;     //On passe à l'arrêt suivant
+		prec1 = a1;       //le précédent de a1 devient a1
 	}
 }
 
-Liste::~Liste()
+Liste::~Liste()     //Destructeur
 {
-    Arret * crt = getTeteArret()->d_arretSuiv;
+    Arret * crt = getTeteArret()->d_arretSuiv;  //On crée un arrêt courant
 
-    while(crt != 0)
+    while(crt != 0)     //Tant que cet arrêt n'est pas nul
     {
-        if(crt->d_arretSuiv == 0)
+        if(crt->d_arretSuiv == 0)   //Si l'arrêt suivant est nul
         {
-            delete crt;
+            delete crt; //On supprime l'arrêt courant
         }
-        else
+        else    //Sinon
         {
-            delete crt->d_arretPrec;
-            crt = crt->d_arretSuiv;
+            delete crt->d_arretPrec;    //On supprime l'arrêt précédent 
+            crt = crt->d_arretSuiv;     //On avance l'arrêt courant
         }
     }
 }
 
 Arret * Liste::getTeteArret() const
 {
-	return d_arretTete;
+	return d_arretTete;    //Retourne l'arrêt de tête
 }
 
 Arret * Liste::getQueueArret() const
 {
-	return d_arretQueue;
+	return d_arretQueue;   //Retourne l'arrêt de queue
 }
 
 void Liste::setTeteArret(Arret &arretTete)
 {
-	*d_arretTete = arretTete;
+	*d_arretTete = arretTete;  //Modifie l'arrêt de tête par celui passé en paramètre
 }
 
 void Liste::setQueueArret(Arret &arretQueue)
 {
-    arretQueue.d_arretPrec = d_arretQueue; // le precedent du nouveau chainon pointe vers la queue de liste
-    d_arretQueue->d_arretSuiv = &arretQueue; // le chainon ar devient la queue
-    arretQueue.d_arretSuiv = 0;
-    d_arretQueue = &arretQueue;
+    arretQueue.d_arretPrec = d_arretQueue; //Le précédent de l'arrêt passé en paramètre pointe vers la queue de la liste
+    d_arretQueue->d_arretSuiv = &arretQueue; // Le suivant de l'ancienne queue devient l'arrêt passé en paramètre
+    arretQueue.d_arretSuiv = 0; //Le suivant de la queue est nul
+    d_arretQueue = &arretQueue; //La nouvelle queue devient celle passée en paramètre
 }
 
 // A refaire (�a fait des erreurs)
 void Liste::insererEnQueue(Arret& ar) // ajouter les exceptions
 {
-    if (this->estVide())
+    if (this->estVide())    //Si la liste d'arrêts courant est vide
     {
-        d_arretTete = &ar;
+        d_arretTete = &ar;  //Alors la tête et la queue pointent sur l'arrêt passé en paramètre
         d_arretQueue = &ar;
 
     }
-    else
+    else    //Sinon
     {
-        setQueueArret(ar);
+        setQueueArret(ar);  //l'arrêt passé en paramètre devient la queue
     }
-    d_taille++;
+    d_taille++;     //On incrémente la taille de la liste d'arrêts
 }
 
 bool Liste::estVide() const
 {
-    return (d_arretTete == nullptr);
+    return (d_arretTete == nullptr);    //renvoie vrai si la liste est vide
 }
 
 int Liste::getTaille() const
 {
-    return  d_taille;
+    return  d_taille;   //Renvoie la taille de la liste d'arrêts
 }
 
-Arret *Liste::getArret(int n) const
+Arret *Liste::getArret(int n) const     //Renvoie un arrêt en fonction de son numéro
 {
-    Arret * crt = getTeteArret();
+    Arret * crt = getTeteArret();   //On crée un arrêt courant pour parcourir la liste sans perdre la tête
 
-    while(crt != 0 && crt->getIdArret() != n)
+    while(crt != 0 && crt->getIdArret() != n)   //tant que nous ne sommes pas arrivés à la queue et que le numéro ne correspond pas à celui passé en paramètre
     {
-        crt = crt->getArretSuivant();
+        crt = crt->getArretSuivant();   //On avance dans la liste
     }
 
-    return crt;
+    return crt; //On retourne l'arrêt
 }
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -17,69 +17,69 @@
 
 void lire(const std::string &nomFichier, std::vector<Ligne> &tabLigne)
 {
-    std::ifstream f(nomFichier.c_str());
+    std::ifstream f(nomFichier.c_str());    //Création de la variable du fichier
 
     int nbLignes;
     char poubelle;
 
-    f >> nbLignes;
+    f >> nbLignes;  //On récupère le nombre de lignes de tram qu'il y a
 
-    tabLigne.reserve(nbLignes);
+    tabLigne.reserve(nbLignes);     //On réserve l'espace mémoire en fonction du nombre de lignes
 
     int indice = -1;
 
-    while(!f.eof())
+    while(!f.eof())     //Tant que ce n'est pas la fin du fichier ...
     {
         std::string s;
 
-        f >> s;
+        f >> s;     //On met à la poubelle la première ligne car on l'a déjà au dessus
         //std::cout << s << std::endl;
 
-        if(s == "#L")
+        if(s == "#L")   //Si le début du ligne commence par #L (donc est une ligne)
         {
             int numLigne ;
-            f >> numLigne;
+            f >> numLigne;      //On récupère son numéro de ligne
             Ligne *ligne = new Ligne(numLigne); // creation de la ligne avec son numero en param
 
             // Creation de la liste d'arrets
             Liste *listeArrets = new Liste();
-            ligne->setListeArret(listeArrets);
+            ligne->setListeArret(listeArrets);  //On ajoute la liste d'arrêts à la ligne
 
             // Creation de la file de trams
             File *fileSensAllerTr = new File();
             File *fileSensRetourTr = new File();
-            ligne->setSensFileAller(fileSensAllerTr);
-            ligne->setSensFileRetour(fileSensRetourTr);
+            ligne->setSensFileAller(fileSensAllerTr);   //On ajoute la file de sens aller à la ligne
+            ligne->setSensFileRetour(fileSensRetourTr); //On ajoute la file de sens retour à la ligne
 
-            tabLigne.push_back(*ligne);
+            tabLigne.push_back(*ligne);     //On stocke la ligne dans le tableau de lignes
             indice++;
         }
-        else if(s == "#A")
+        else if(s == "#A")  //Sinon si le début de ligne commence par #A (donc un arrêt) 
         {
-            Arret *ar = new Arret();
-            std::string libelle;
-            int id, posx, posy,tempsArret;
-            f >> id >>libelle >> posx >> posy >> tempsArret;
-            ar->setIdArret(id);
+            Arret *ar = new Arret();    //On crée un arrêt
+            std::string libelle;    
+            int id, posx, posy,tempsArret;  //On initialise des variables pour récupérer les informations
+            f >> id >>libelle >> posx >> posy >> tempsArret; //On récupères les infos
+            ar->setIdArret(id);     //On les stocke dans chaque variable 
             ar->setLibelle(libelle);
             ar->setPotistion(posx, posy);
             ar->setTempsArret(tempsArret);
-            tabLigne[indice].getListeArret().insererEnQueue(*ar); // ajout de l'arret en queue de liste
+            tabLigne[indice].getListeArret().insererEnQueue(*ar); // Ajout de l'arret en queue de liste
         }
-        else if(s == "#T")
+        else if(s == "#T")  //Sinon si le début de la ligne commence par #T (donc un tram)
         {
-            Tram *tr = new Tram;
+            Tram *tr = new Tram;    //On crée un nouveau tram
 
-            bool vitTr;
+            bool vitTr;     //On initialise chaque variable pour récupérer les infos
             int posXTr;
             int posYTr;
             bool sensTr;
             int numLigneTr;
             int numArretSuiv;
 
-            f >> vitTr >> posXTr >> posYTr >> sensTr >> numLigneTr >> numArretSuiv;
+            f >> vitTr >> posXTr >> posYTr >> sensTr >> numLigneTr >> numArretSuiv;     //On récupère les infos
 
-            tr->setVitesse(vitTr);
+            tr->setVitesse(vitTr);      //On stocke les infos dans les variables
             tr->setPosition(posXTr, posYTr);
             tr->setSens(sensTr);
             tr->setNumLigne(numLigneTr);
@@ -87,16 +87,16 @@ void lire(const std::string &nomFichier, std::vector<Ligne> &tabLigne)
             tr->setArretSuivant(*tabLigne[indice].getListeArret().getArret(numArretSuiv));
 
 
-            if(tr->getSens())	// sens aller
+            if(tr->getSens())	// Si le sens est true alors sens aller
             {
                 tabLigne[indice].getSensFileAller().entrer(*tr);
             }
-            else
+            else    //Sinon sens retour
             {
                 tabLigne[indice].getSensFileRetour().entrer(*tr);
             }
         }
-        else
+        else    //Si on ne tombe sur aucun des cas précédents alors erreur
         {
             std::cout << "error";
         }
@@ -110,59 +110,59 @@ void testAfficherLigne(std::vector<Ligne> &tabLigne)
 {
     int indice = 0;
     std::cout << "Arrets : " << std::endl;
-    Arret *arretCourant = new Arret();
-    arretCourant = tabLigne[indice].getListeArret().getTeteArret();
-    for(int i = 0; i < tabLigne[indice].getListeArret().getTaille(); i++)
+    Arret *arretCourant = new Arret();      //On crée un nouvel arrêt pour pouvoir parcourir la liste d'arrêts sans perdre la tête
+    arretCourant = tabLigne[indice].getListeArret().getTeteArret(); //arretCourant = arret de tête
+    for(int i = 0; i < tabLigne[indice].getListeArret().getTaille(); i++)   //Pour i allant de la tête au dernier arrêt
     {
 
-        std::cout << arretCourant->getIdArret() << " "
+        std::cout << arretCourant->getIdArret() << " "      //On affiche les informations concernant cet arrêt
                   << arretCourant->getLibelle() << " "
                   << arretCourant->getPosition().getX() << " "
                   << arretCourant->getPosition().getY() << " "
                   << arretCourant->getTempsArret() << std::endl;
 
 //        std::cout << tabLigne[indice].arrets[i].nomArret << " " << tabLigne[indice].arrets[i].posx << " " << tabLigne[indice].arrets[i].posy << " " << tabLigne[indice].arrets[i].tempsArret << std::endl;
-        arretCourant = arretCourant->getArretSuivant();
+        arretCourant = arretCourant->getArretSuivant();     //On passe à l'arrêt suivant
     }
 
 
     std::cout << "Trams : " << std::endl;
 
-    Tram* tramCourant = new Tram();
-    tramCourant = tabLigne[indice].getSensFileAller().getPremierTram();
-    for(int i = 0; i < tabLigne[indice].getSensFileAller().getTaille(); i++)
+    Tram* tramCourant = new Tram();     //On crée un tram courant pour pouvoir parcourir la file de trams
+    tramCourant = tabLigne[indice].getSensFileAller().getPremierTram(); //tramCourant = premier tram (tête)
+    for(int i = 0; i < tabLigne[indice].getSensFileAller().getTaille(); i++)    //Pour i allant de la tête au dernier tram
     {
 
 
-        std::cout << tramCourant->getVitesse() << " "
+        std::cout << tramCourant->getVitesse() << " "       //On affiche toutes les informations de ce tram
                   << tramCourant->getPosition().getX() << " "
                   << tramCourant->getPosition().getY() << " "
                   << tramCourant->getSens() << " "
                   << tramCourant->getNumLigne() << " "
                   << tramCourant->getNumArretSuivant() <<  std::endl;
-        tramCourant = tramCourant->getTramSuivant();
+        tramCourant = tramCourant->getTramSuivant();    //On passe au tram suivant
     }
     indice ++ ;
 }
 
 void afficherReseau(const std::vector<Ligne> &tabDeLignes)
 {
-    for(int i = 0; i < tabDeLignes.size(); i++)
+    for(int i = 0; i < tabDeLignes.size(); i++)     // Pour i allant de la première ligne à la dernière
     {
-        Arret * arCrt = tabDeLignes[i].getListeArret().getTeteArret();
+        Arret * arCrt = tabDeLignes[i].getListeArret().getTeteArret();  //On crée un arrêt courant = tête arrêt
 
-        for(int j = 0; j < tabDeLignes[i].getListeArret().getTaille(); j++)
+        for(int j = 0; j < tabDeLignes[i].getListeArret().getTaille(); j++)     //Pour j allant du premier arrêt au dernier
         {
-            bar(arCrt->getPosition().getX()-5, arCrt->getPosition().getY()-5,
+            bar(arCrt->getPosition().getX()-5, arCrt->getPosition().getY()-5,   //On dessine un rectangle de 5x5 pour représenter l'arrêt et on le place à la bonne position
                 arCrt->getPosition().getX()+5, arCrt->getPosition().getY()+5);
 
-            if(arCrt->getArretSuivant() != 0)
+            if(arCrt->getArretSuivant() != 0)      //Si on n'est pas à la queue (si il y a encore un arrêt après)
             {
-                line(arCrt->getPosition().getX(), arCrt->getPosition().getY(),
+                line(arCrt->getPosition().getX(), arCrt->getPosition().getY(),      //On trace une ligne qui relie les deux arrêts (actuel et suivant)
                      arCrt->getArretSuivant()->getPosition().getX(), arCrt->getArretSuivant()->getPosition().getY());
             }
 
-            arCrt = arCrt->getArretSuivant();
+            arCrt = arCrt->getArretSuivant();   //On passe à l'arrêt suivant
         }
 
     }
@@ -170,23 +170,23 @@ void afficherReseau(const std::vector<Ligne> &tabDeLignes)
 
 void afficherTrams(const std::vector<Ligne> &tabDeLignes)
 {
-    for(int i = 0; i < tabDeLignes.size(); i++)
+    for(int i = 0; i < tabDeLignes.size(); i++) //Pour i allant de la première ligne à la dernière
     {
-        Tram * trCrtAller = tabDeLignes[i].getSensFileAller().getPremierTram();
-        Tram * trCrtRetour = tabDeLignes[i].getSensFileRetour().getPremierTram();
+        Tram * trCrtAller = tabDeLignes[i].getSensFileAller().getPremierTram(); //On crée un tram courant pour le sens aller = tête tram aller 
+        Tram * trCrtRetour = tabDeLignes[i].getSensFileRetour().getPremierTram();   //On crée un tram courant pour le sens retour = tête tram retour
 
-        for(int j = 0; j < tabDeLignes[i].getSensFileAller().getTaille(); j++)
+        for(int j = 0; j < tabDeLignes[i].getSensFileAller().getTaille(); j++)  //Pour j allant du premier tram de la file aller au dernier
         {
-            circle(trCrtAller->getPosition().getX(), trCrtAller->getPosition().getY(), 10);
+            circle(trCrtAller->getPosition().getX(), trCrtAller->getPosition().getY(), 10); //On dessine un cercle de rayon 10 pour le représenter et on le place à la bonne position
 
-            trCrtAller = trCrtAller->getTramSuivant();
+            trCrtAller = trCrtAller->getTramSuivant();  //On passe au tram suivant
         }
 
-        for(int j = 0; j < tabDeLignes[i].getSensFileRetour().getTaille(); j++)
+        for(int j = 0; j < tabDeLignes[i].getSensFileRetour().getTaille(); j++) //Pour j allant du premier tram de la file retour au dernier
         {
-            circle(trCrtRetour->getPosition().getX(), trCrtRetour->getPosition().getY(), 10);
+            circle(trCrtRetour->getPosition().getX(), trCrtRetour->getPosition().getY(), 10);   //On dessine un cercle de rayon 10 et on le place à la bonne position
 
-            trCrtRetour = trCrtRetour->getTramSuivant();
+            trCrtRetour = trCrtRetour->getTramSuivant();    //On passe au tram suivant
         }
     }
 }
