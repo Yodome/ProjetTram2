91822affb00dc5d0841e8c7ee978e6c59fae900a
Yodome <van_minh_nguyen@hotmail.com>
2018-03-18 21:24:09 +0100

    Add files via upload
    Ajout des commentaires pour la documentation Doxygen des classes Ligne et Arret

---
+++ Arret.h
@@ -0,0 +1,69 @@
+//
+// Created by e1701567 on 20/02/2018.
+//
+
+#ifndef PROJETTRAM_ARRET_H
+#define PROJETTRAM_ARRET_H
+
+
+#include <string>
+#include "Position.h"
+
+
+class Arret
+{
+public:
+    // Constructeur par défaut
+	Arret();
+
+    // Constructeur avec données en paramètre
+	Arret(int idArret, int tempsArretMin, std::string libelle, Position pos);
+
+
+    //------------------- GETTER ---------------------
+	// Renvoie la position de l'arrêt
+    Position& getPosition();
+
+    // Renvoie le temps d'arret minimum sur l'arrêt
+	int getTempsArret() const;
+
+    // Renvoie le numéro de l'arrêt
+    int getIdArret() const;
+
+    // Renvoie le nom de l'arrêt
+    std::string getLibelle() const;
+
+    // Renvoie l'arrêt suivant
+    Arret* getArretSuivant() const;
+
+    // Renvoie l'arrêt précédent
+    Arret* getArretPrecedent() const;
+
+
+    //------------------- SETTER ---------------------
+    // Modifie le nom de l'arrêt
+    void setLibelle(std::string nom);
+
+    // Modifie la position de l'arrêt
+    void setPotistion(int x, int y);
+
+    // Modifie le temsp d'arret minimum d'un tram sur l'arrêt
+    void setTempsArret(int temps);
+
+    // Modifie le numéro de l'arrêt
+    void setIdArret(int id);
+
+
+	friend class Liste;
+private:
+    int d_idArret; // numéro de l'arrêt
+	int d_tempsArretMin; // temps d'arret minimum d'un tram sur l'arrêt
+	std::string d_libelle; // nom de l'arrêt
+	Position d_position; // position de l'arrêt
+	Arret *d_arretSuiv; // arrêt suivant dans la liste
+	Arret *d_arretPrec; // arrêt précédent dans la liste
+
+};
+
+
+#endif //PROJETTRAM_ARRET_H
---
+++ Ligne.cpp
@@ -0,0 +1,87 @@
+//
+// Created by e1701567 on 20/02/2018.
+//
+
+#include "..\Headers\Ligne.h"
+
+
+/**
+ * Constructeur de ligne avec des paramètres
+ * @param [in] num - numéro de la ligne
+ */
+Ligne::Ligne(int num) : d_num{ num }, d_listeArret{ nullptr }, d_fileSensAller{ nullptr },
+d_fileSensRetour{ nullptr }
+{
+
+}
+
+//--------------------------------- GETTER -------------------------------
+/**
+ * Renvoie le numéro de la ligne
+ * @return d_num - numéro de la ligne
+ */
+int Ligne::getLigne() const
+{
+	return d_num;
+}
+
+/**
+ * Renvoie la liste des arrêts de la ligne
+ * @return d_listeArret - la liste de tous les arrêts de la ligne
+ */
+Liste &Ligne::getListeArret() const
+{
+	return *d_listeArret;
+}
+
+/**
+ * Renvoie la file de tram dans le sens aller
+ * @return d_fileSensAller - file de tram du sens aller
+ */
+File &Ligne::getSensFileAller() const
+{
+	return *d_fileSensAller;
+}
+
+/**
+ * Renvoie la file de tram dans le sens retour
+ * @return d_fileSensRetour - file de tram du sens retour
+ */
+File &Ligne::getSensFileRetour() const
+{
+	return *d_fileSensRetour;
+}
+
+
+//--------------------------------- SETTER -------------------------------
+/**
+ * Modifie le numéro de la ligne
+ * @param [in] num - numéro de la ligne
+ */
+void Ligne::setLigne(int num) {
+	d_num = num;
+}
+
+/**
+ * Modifie la liste des arrêts de la ligne
+ * @param [in] listeArret - nouvelle liste d'arrêt
+ */
+void Ligne::setListeArret(Liste *listeArret) {
+	d_listeArret = listeArret;
+}
+
+/**
+ * Modifie la file de tram de l'aller
+ * @param [in] sensFileAller - nouvelle file de tram pour le sens aller
+ */
+void Ligne::setSensFileAller(File *sensFileAller) {
+	d_fileSensAller = sensFileAller;
+}
+
+/**
+ * Modifie la file de tram du retour
+ * @param [in] sensFileRetour - nouvelle file de tram pour le sens retour
+ */
+void Ligne::setSensFileRetour(File *sensFileRetour) {
+	d_fileSensRetour = sensFileRetour;
+}
