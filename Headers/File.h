32b9f97558d74da02d740569b03e22c691660f1f
OwDemShad <galo.guccy@gmail.com>
2018-03-18 18:11:45 +0100

    Ajout de commentaires sur File.h et Tram.h

--- Headers/File.h
+++ Headers/File.h
@@ -7,24 +7,34 @@
 class File
 {
 public:
+	// Constructeur par défaut
 	File();
+	// Destructeur --> appel de sortir() tant que la file !estVide()
 	~File();
+	// Renvoie le premier tram
 	Tram * getPremierTram() const;
+	// Renvoie le dernier tram
 	Tram * getDernierTram() const;
 
+	// Modifie le premier tram
 	void setPremierTram(Tram& premierTram);
+	// Modifie le dernier tram
 	void setDernierTram(Tram& dernierTram);
 
+	// Retourne la taille de la file
 	int getTaille() const;
 
+	// Permet d'entrer un tram tr dans la file en dernier
 	void entrer(Tram &tr);
+	// Fait sortir le tram en tête de la file
 	Tram& sortir();
+	// Retourne vrai si la file est vide --> si tete == queue
 	bool estVide() const;
 
 private:
-	int d_taille;
-	Tram * d_premierTram;
-	Tram *d_dernierTram;
+	int d_taille;	// taille de la file
+	Tram * d_premierTram;	// pointeur sur le premier tram
+	Tram *d_dernierTram;	// pointeur sur le dernier tram
 };
 
 
--- Headers/Tram.h
+++ Headers/Tram.h
@@ -13,50 +13,77 @@
 class Tram
 {
 public:
+	// Constructeur par défaut
 	Tram();
 
-	//getter
+	//------------------- GETTER ---------------------
+	// Renvoie la vitesse actuelle du tram
 	bool getVitesse() const;
+	// Renvoie la vitesse max du tram
 	int getVitessemax() const;
+	// Renvoie le sens du tram (1 pour gauche-droite, 0 pour droite-gauche)
 	bool getSens() const;
+	// Renvoie le temps restant que le tram doit passer à l'arret
 	int getTempsArret() const;
+	// Renvoie la distance minimale à respecter entre deux trams
 	int getDistanceMin() const;
+	// Renvoie le numéro de ligne auquel le tram est affilié
 	int getNumLigne() const;
+	// Renvoie la position actuelle du tram
 	Position getPosition();
+	// Renvoie un pointeur sur le tram suivant (celui de devant)
 	Tram* getTramSuivant() const;
+	// Renvoie un pointeur sur l'arrêt vers lequel se dirige le tram
 	Arret* getArretSuivant() const;
+	// Renvoie le numéro de l'arrêt vers lequel se dirige le tram
     int getNumArretSuivant() const;
 
-	//setter
+	//------------------ SETTER -----------------------
+	// Modifie la vitesse du tram
 	void setVitesse(bool vitesse);
+	// Modifie la vitesse maximale du tram
 	void setVitessemax(int vitesseMax);
+	// Modifie le sens du tram
 	void setSens(bool sens);
+	// Modifie le temps restant que le tram doit passer sur l'arrêt
 	void setTempsArret(int tempsArret);
+	// Modifie la distance minimale que le tram doit respecter
 	void setDistanceMin(int distanceMin);
+	// Modifie le numéro de la ligne auquel le tram est affilié
 	void setNumLigne(int numLigne);
+	// Modifie la position actuelle du tram
 	void setPosition(int x, int y);
+	// Modifie le pointeur sur le tram suivant
 	void setTramSuivant(Tram& tramSuivant);
+	// Modifie le pointeur sur l'arrêt suivant (celui vers lequel le tram se dirige)
 	void setArretSuivant(Arret& arretSuivant);
+	// Modifie le numéro de l'arrêt vers lequel le tram se dirige
     void setNumArretSuivant (int numArretSuivant);
 
-	void avance();	// d�termine la position du tram selon sa vitesse
+
+	// Détermine la position du tram selon sa vitesse
+	void avance();
+	// Renvoie la distance avec le tramSuivant
 	double distanceTramDevant() const;
+	// Renvoie la distance avec arretSuivant
 	double distanceArretSuiv() const;
-	bool doitSArreter();	// indique si le tram doit s'arr�ter (ne proc�de � aucun changement des coordonn�es du tram)
+	// Indique si le tram doit s'arr�ter (ne proc�de � aucun changement des coordonn�es du tram)
+	bool doitSArreter();
+	// Met le tram courant à l'arrêt : sa vitesse devient nulle
 	void arret();
 
 	friend class File;
 private:
-	bool d_vitesse;
-	int d_vitesseMax;
-	bool d_sens;
-	int d_tempsArret;
-	int d_distanceMin;
-	int d_numLigne;
-	int d_numArretSuiv;
-	Position d_position;
-	Tram *d_tramSuiv;
-	Arret *d_arretSuiv;//friend, on veut juste avoir le prochain
+	bool d_vitesse;	// vitesse actuelle du tram
+	int d_vitesseMax;	// vitesse maximale du tram
+	bool d_sens;	// sens du tram
+	int d_tempsArret;	// temps d'arrêt restant que le tram doit passer
+	int d_distanceMin;	// distance minimale que le tram doit respecter
+	int d_numLigne;		// numéro de ligne auquel le tram est affilié
+	int d_numArretSuiv;	// numéro de l'arrêt suivant vers lequel le tram se dirige
+	Position d_position;	// position du tram
+	Tram *d_tramSuiv;	// tram suivant dans la file
+	Arret *d_arretSuiv;		// arret suivant vers lequel le tram se dirige
 };
 
 
